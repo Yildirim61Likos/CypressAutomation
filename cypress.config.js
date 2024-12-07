17b759d30de1c4a19937ae8a9b0904229787c36a
Yildirim <yildirim61likos@gmail.com>
2024-12-07 12:19:39 -0800

    Updated

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig(
   e2e: {
     setupNodeEvents(on, config) 
     {
+      specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
 
       require('cypress-mochawesome-reporter/plugin')(on);// HTML REPORT !!!
       // implement node event listeners here
